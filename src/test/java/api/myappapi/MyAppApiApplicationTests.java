7faeb8f230961acd0c46a70e191e9a455a542932
Matthew Clark <Everdein@gmail.com>
2022-08-05 23:29:06 -0500

    Initial commit

---
+++ src/test/java/api/myappapi/MyAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package api.myappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
