d46c9d33834e2493a63b3a69b2f28dcbb5c01002
Matthew <everdein@gmail.com>
2022-08-07 18:55:39 -0500

    Student Class

--- src/main/java/api/myappapi/MyAppApiApplication.java
+++ src/main/java/api/myappapi/MyAppApiApplication.java
@@ -1,13 +1,35 @@
 package api.myappapi;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import api.myappapi.student.Student;
+
 
 @SpringBootApplication
+@RestController // Serves REST endpoints
 public class MyAppApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyAppApiApplication.class, args);
 	}
 
+	@GetMapping // REST endpoint
+	public List<Student> hello() {
+		return List.of(
+			new Student(
+				1L, 
+				"miriam", 
+				"miriam@gmail.com", 
+				LocalDate.of(2000, Month.JANUARY, 5), 
+				18
+			)
+		);
+	}
 }
